91e203ef69c9f547c2a3bf74e5a9b932e7bed0c2
Caio Ferreira <andradefcaio@gmail.com>
2024-01-10 19:12:42 -0300

    Revert "Par ou ímpar"
    This reverts commit 263126980dd3b2a305cc281623e4c2efc2d7efe1.

--- Par ou impar.py
+++
@@ -1,6 +0,0 @@
-numero = int(input('Digite um número: '))
-resultado = numero % 2
-if resultado == 0:
-    print('O número {} é par'.format(numero))
-else:
-    print('O numero {} é impar'.format(numero))
